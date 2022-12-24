115eb7f3c4ce5e766302f9f8ae14e84595f14409
Rusrty <3130585774@qq.com>
2022-12-24 21:13:49 +0800

    添加项目文件。

---
+++ Utils/ElementHandleExtensions.cs
@@ -0,0 +1,22 @@
+﻿using PuppeteerSharp;
+using System.Threading.Tasks;
+
+static class ElementHandleExtensions
+{
+    public static Task<string> GetAttributeAsync(this ElementHandle element, string attributeName)
+    {
+        return element.EvaluateFunctionAsync<string>($"(item)=>item.getAttribute('{attributeName}')");
+    }
+
+    public static async Task<string> GetOuterHtmlAsync(this ElementHandle element)
+    {
+        var outerHtmlHandle = await element.GetPropertyAsync("outerHTML");
+        return await outerHtmlHandle.JsonValueAsync<string>();
+    }
+
+    public static async Task<string> GetInnerTextAsync(this ElementHandle element)
+    {
+        var outerHtmlHandle = await element.GetPropertyAsync("innerText");
+        return await outerHtmlHandle.JsonValueAsync<string>();
+    }
+}
